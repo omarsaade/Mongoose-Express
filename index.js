55f1062ee203a396a4f79e0670f7416e69826f1c
omar saadeh <omarsaadeh11@gmail.com>
2022-12-07 12:03:43 -0800

    Mongoose

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const courseSchema = new mongoose.Schema({
 });
 
 /*
-now we need to compile this schema into a model
+Now we need to compile this schema into a model
      john hye instance men el class human
         Classes   , objects
          Human    , John
@@ -21,12 +21,34 @@ now we need to compile this schema into a model
 */
 
 // Course is a class
+//to create a class like course we need to compile the schema to model
 const Course = mongoose.model("Course", courseSchema);
 
-//object course
-const course = new Course({
-  name: "Node.js Course",
-  author: "Mosh",
-  tags: ["node", "backend"],
-  isPublished: true,
-});
+async function createCourse() {
+  //object course
+  const course = new Course({
+    name: "Mosh Course",
+    author: "Maximillian shwarzmuller",
+    tags: ["node", "backend"],
+    isPublished: true,
+  });
+
+  //  Async operation , save
+  // The save() method returns a promise. If save() succeeds, the promise resolves to the document that was saved.
+  const result = await course.save();
+  //display on the console
+  console.log(result);
+}
+
+async function getCourses() {
+  //.find() btarje3 document query object..li hye promise
+  const courses = await Course.find({ author: "Mosh", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    // 1 ASCENDING ORDER   DESC -1
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+
+// getCourses();
+createCourse();
