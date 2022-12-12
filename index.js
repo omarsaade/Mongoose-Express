d0fc54b46ccfbf523fa683390c336b9250a9dd4f
omar saadeh <omarsaadeh11@gmail.com>
2022-12-12 12:19:17 -0800

    Mongoose

--- index.js
+++ index.js
@@ -12,43 +12,51 @@ const courseSchema = new mongoose.Schema({
   isPublished: Boolean,
 });
 
-/*
-Now we need to compile this schema into a model
-     john hye instance men el class human
-        Classes   , objects
-         Human    , John
-         Course   , nodeCourse
-*/
-
-// Course is a class
-//to create a class like course we need to compile the schema to model
 const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
-  //object course
   const course = new Course({
-    name: "Mosh Course",
+    name: "Mosh",
     author: "Maximillian shwarzmuller",
     tags: ["node", "backend"],
     isPublished: true,
   });
 
-  //  Async operation , save
-  // The save() method returns a promise. If save() succeeds, the promise resolves to the document that was saved.
   const result = await course.save();
-  //display on the console
+
   console.log(result);
 }
+// createCourse();
 
 async function getCourses() {
-  //.find() btarje3 document query object..li hye promise
-  const courses = await Course.find({ author: "Mosh", isPublished: true })
+  const courses = await Course
+    // .find({
+    //   name: "Mosh",
+    //   isPublished: true,
+    // })
+    // .find({ price: { $gt: 10, $lte: 20 } })
+    .find({ price: { $in: [10, 15, 20] } })
     .limit(10)
     .sort({ name: 1 })
-    // 1 ASCENDING ORDER   DESC -1
     .select({ name: 1, tags: 1 });
   console.log(courses);
 }
 
-// getCourses();
-createCourse();
+getCourses();
+
+/*
+
+eq(equal)
+ne(not equal)
+gt (greater than)
+gte (greater than or equal to)
+lt (less than)
+lte (less than or equal to)
+in
+nin (not in)
+
+
+lets imagine our courses have a price property
+
+
+*/
