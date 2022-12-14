d2d948855c8f5c1336341f605a4746850f70af74
omar saadeh <omarsaadeh11@gmail.com>
2022-12-14 04:52:39 -0800

    CRUD Operations Using MongoDB

--- All.database.js
+++ All.database.js
@@ -968,3 +968,68 @@ removeCourse("63975c4e8d6ada1f00c44029");
 // removeCourse("63975cd920f8301ff88d62e4");
 
 
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to Mongodb..."))
+  .catch((err) => console.log("Could not connect to mongodb...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function removeCourse(id) {
+  //   const result = await Course.deleteMany({isPublished: true });
+  const course = await Course.findByIdAndRemove(id);
+  console.log(course);
+  //   const course = await Course.deleteMany({ _id: id });
+  //   console.log(course);
+}
+removeCourse("63975c4e8d6ada1f00c44029");
+/*
+
+
+
+
+
+
+//                       delete one
+
+
+
+
+*/
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to Mongodb..."))
+  .catch((err) => console.log("Could not connect to mongodb...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function removeCourse(id) {
+  const course = await Course.deleteOne({ _id: id });
+  console.log(course);
+}
+removeCourse("63975cd920f8301ff88d62e4");
+
+//iza mafi course bi hal id btraje3 null
